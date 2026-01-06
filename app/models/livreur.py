3a370b4204302e66a981dbebaa9eea8aafe03567
Bouchta Mohamed <bouchtamohamed01@gmail.com>
2026-01-06 15:59:30 +0100

    Ajout du modele Livreur

--- app/models/livreur.py
+++ app/models/livreur.py
@@ -1,1 +1,12 @@
-# Modèle Livreur
+from sqlalchemy import Column, Integer, String
+from core.config import Base
+
+class Livreur(Base):
+    __tablename__ = "livreurs"
+
+    id = Column(Integer, primary_key=True)
+    nom = Column(String, nullable=False)
+    prenom = Column(String, nullable=False)
+    telephone = Column(String, nullable=False)
+    vehicule = Column(String, nullable=True)
+    zone_assignee = Column(String, nullable=True)
