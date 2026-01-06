d38a32388afe378484a4062fc76ffb578b9f2c0b
Bouchta Mohamed <bouchtamohamed01@gmail.com>
2026-01-06 15:59:20 +0100

    Ajout du modele Destinataire

--- app/models/destinataire.py
+++ app/models/destinataire.py
@@ -1,1 +1,12 @@
-# Modèle Destinataire
+from sqlalchemy import Column, Integer, String
+from core.config import Base
+
+class Destinataire(Base):
+    __tablename__ = "destinataires"
+
+    id = Column(Integer, primary_key=True)
+    nom = Column(String, nullable=False)
+    prenom = Column(String, nullable=False)
+    email = Column(String, unique=True, nullable=False)
+    telephone = Column(String, nullable=False)
+    adresse = Column(String, nullable=False)
