27939e537f1ebc2f2cbe152052bd4d5bf3f8e3a6
Bouchta Mohamed <bouchtamohamed01@gmail.com>
2026-01-09 22:38:57 +0100

    fix: corrections et améliorations dans les routes client

--- app/routes/client_routes.py
+++ app/routes/client_routes.py
@@ -11,7 +11,12 @@ router = APIRouter(
 
 @router.post("/",response_model=ClientExpediteurRead,status_code=status.HTTP_201_CREATED)
 def create_client_route(client: ClientExpediteurCreate,db: Session = Depends(get_db)):
-    return create_client(db,client)
+    try:
+        return create_client(db,client)
+    except ValueError as e:
+        raise HTTPException(status_code=400,detail=str(e))
+        
+    
 
 @router.get("/",response_model=list[ClientExpediteurRead])
 def get_client_route(db:Session = Depends(get_db)):
@@ -20,22 +25,22 @@ def get_client_route(db:Session = Depends(get_db)):
 
 @router.get("/{client_id}", response_model=ClientExpediteurRead)
 def get_client_by_id_route(client_id: int, db: Session = Depends(get_db)):
-    client = get_clients_by_id(db, client_id)
-    if not client:
-        raise HTTPException(status_code=404, detail="Client not found")
-    return client
+    try:
+        return get_clients_by_id(db, client_id)
+    except ValueError as e:
+        raise HTTPException(status_code=404,detail=str(e))
 
 @router.put("/{client_id}", response_model=ClientExpediteurRead)
 def update_client_route(client_id: int, client_update: ClientExpediteurUpdate, db: Session = Depends(get_db)):
-    client = update_client(db, client_id, client_update)
-    if not client:
-        raise HTTPException(status_code=404, detail="Client not found")
-    return client
-
+    try:
+        return update_client(db, client_id, client_update)
+    except ValueError as e:
+        raise HTTPException(status_code=400,detail=str(e))
 
 @router.delete("/{client_id}", status_code=status.HTTP_204_NO_CONTENT)
 def delete_client_route(client_id: int, db: Session = Depends(get_db)):
-    client = delete_client(db, client_id)
-    if not client:
-        raise HTTPException(status_code=404, detail="Client not found")
-    return None
+    try:
+        return delete_client(db, client_id)
+    except ValueError as e:
+        raise HTTPException(status_code=404,detail=str(e))
+
